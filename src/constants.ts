ee34bbe573b0fada5ff38103719d45476ff7c861
Angel Dominguez <angeldomp49@gmail.com>
2022-07-01 16:47:18 -0500

    CodeProcessor

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,7 @@
 export const CONFIG_FILE = "tezcatl.config.json";
 export const TEMPLATE_EXTENSION = ".tzl";
 export const TEMPLATE_DESCRIPTOR = "tezcatl.templates.json";
+
+export interface CodeProcessor{
+    parse(code: string): string;
+}
