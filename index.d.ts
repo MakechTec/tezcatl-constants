fe7e46b5692fae32a8b68126c9c4313170c2f8e3
Angel Dominguez <angeldomp49@gmail.com>
2022-07-01 16:50:47 -0500

    declaration file

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+export const CONFIG_FILE: "tezcatl.config.json";
+export const TEMPLATE_EXTENSION: ".tzl";
+export const TEMPLATE_DESCRIPTOR: "tezcatl.templates.json";
+export interface CodeProcessor {
+    parse(code: string): string;
+}
